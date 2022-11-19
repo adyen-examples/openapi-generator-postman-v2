169efe88c321f228ae0e031a48c98f06f534ada0
Beppe Catanese <gcatanese@yahoo.com>
2022-11-19 12:20:38 +0100

    Help method

--- src/test/java/com/tweesky/cloudtools/codegen/PostmanV2GeneratorTest.java
+++ src/test/java/com/tweesky/cloudtools/codegen/PostmanV2GeneratorTest.java
@@ -1,9 +1,12 @@
 package com.tweesky.cloudtools.codegen;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openapitools.codegen.ClientOptInput;
 import org.openapitools.codegen.DefaultGenerator;
 import org.openapitools.codegen.config.CodegenConfigurator;
+
+import static org.junit.Assert.assertEquals;
 
 public class PostmanV2GeneratorTest {
 
