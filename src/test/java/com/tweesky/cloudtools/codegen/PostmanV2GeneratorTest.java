64b3af7f9837643fb5177a74d33468fa9f0f3504
Beppe Catanese <gcatanese@yahoo.com>
2022-12-09 15:52:06 +0100

    Del unused import

--- src/test/java/com/tweesky/cloudtools/codegen/PostmanV2GeneratorTest.java
+++ src/test/java/com/tweesky/cloudtools/codegen/PostmanV2GeneratorTest.java
@@ -16,9 +16,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
