b9d74475a11a7b75e9c02bb9e5dd7f46ca43971d
gcatanese <gcatanese@yahoo.com>
2023-01-19 18:08:52 +0100

    Set Status

--- src/main/java/com/tweesky/cloudtools/codegen/PostmanV2Generator.java
+++ src/main/java/com/tweesky/cloudtools/codegen/PostmanV2Generator.java
@@ -237,6 +237,8 @@ public class PostmanV2Generator extends DefaultCodegen implements CodegenConfig 
       // set all available responses
       for(CodegenResponse codegenResponse : codegenOperation.responses) {
 
+        codegenResponse.vendorExtensions.put("status", getStatus(codegenResponse));
+
         if(requestBody != null) {
           // re-use request body for each response
           codegenResponse.vendorExtensions.put("requestBody", requestBody);
@@ -451,6 +453,52 @@ public class PostmanV2Generator extends DefaultCodegen implements CodegenConfig 
     }
 
     return ret;
+  }
 
+  /**
+   * get HTTP Status Code as text
+   * @param codegenResponse
+   * @return
+   */
+  String getStatus(CodegenResponse codegenResponse) {
+    String ret = "";
+
+    if (codegenResponse.is2xx) {
+      if (codegenResponse.code.equalsIgnoreCase("200")) {
+        ret = "OK";
+      } else if (codegenResponse.code.equalsIgnoreCase("201")) {
+        ret = "Created";
+      } else {
+        ret = "Success";
+      }
+    } else if (codegenResponse.is3xx) {
+      ret = "Redirection";
+    }
+    if (codegenResponse.is4xx) {
+      if (codegenResponse.code.equalsIgnoreCase("400")) {
+        ret = "Bad Request";
+      } else if (codegenResponse.code.equalsIgnoreCase("401")) {
+        ret = "Unauthorized";
+      } else if (codegenResponse.code.equalsIgnoreCase("403")) {
+        ret = "Forbidden";
+      } else if (codegenResponse.code.equalsIgnoreCase("404")) {
+        ret = "Not Found";
+      } else if (codegenResponse.code.equalsIgnoreCase("409")) {
+        ret = "Conflict";
+      } else {
+        ret = "Client Error";
+      }
+    }
+    if (codegenResponse.is5xx) {
+      if (codegenResponse.code.equalsIgnoreCase("500")) {
+        ret = "Internal Server Error";
+      } else if (codegenResponse.code.equalsIgnoreCase("501")) {
+        ret = "Not Implemented";
+      } else {
+        ret = "Server Error";
+      }
+    }
+
+    return ret;
   }
 }
