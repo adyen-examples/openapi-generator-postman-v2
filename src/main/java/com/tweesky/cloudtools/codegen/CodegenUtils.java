2fb6e2bbc5af63bf238cffa7a6d333d5b9ea0f4a
Julien Lengrand-Lambert <julien@lengrand.fr>
2024-02-29 11:12:22 +0100

    Switching to a more general status code list

--- src/main/java/com/tweesky/cloudtools/codegen/CodegenUtils.java
+++ src/main/java/com/tweesky/cloudtools/codegen/CodegenUtils.java
@@ -1,6 +1,9 @@
 package com.tweesky.cloudtools.codegen;
 
+import org.apache.http.impl.EnglishReasonPhraseCatalog;
 import org.openapitools.codegen.CodegenResponse;
+
+import java.net.HttpURLConnection;
 
 public class CodegenUtils {
 
@@ -11,47 +14,10 @@ public class CodegenUtils {
      * @return HTTP Status code information, as text description
      */
     public static String getStatus(CodegenResponse codegenResponse) {
-        String ret = "";
-
-        if (codegenResponse.is2xx) {
-            if (codegenResponse.code.equalsIgnoreCase("200")) {
-                ret = "OK";
-            } else if (codegenResponse.code.equalsIgnoreCase("201")) {
-                ret = "Created";
-            }
-            else if (codegenResponse.code.equalsIgnoreCase("202")) {
-                ret = "Accepted";
-            } else {
-                ret = "Success";
-            }
-        } else if (codegenResponse.is3xx) {
-            ret = "Redirection";
-        }
-        if (codegenResponse.is4xx) {
-            if (codegenResponse.code.equalsIgnoreCase("400")) {
-                ret = "Bad Request";
-            } else if (codegenResponse.code.equalsIgnoreCase("401")) {
-                ret = "Unauthorized";
-            } else if (codegenResponse.code.equalsIgnoreCase("403")) {
-                ret = "Forbidden";
-            } else if (codegenResponse.code.equalsIgnoreCase("404")) {
-                ret = "Not Found";
-            } else if (codegenResponse.code.equalsIgnoreCase("409")) {
-                ret = "Conflict";
-            } else {
-                ret = "Client Error";
-            }
-        }
-        if (codegenResponse.is5xx) {
-            if (codegenResponse.code.equalsIgnoreCase("500")) {
-                ret = "Internal Server Error";
-            } else if (codegenResponse.code.equalsIgnoreCase("501")) {
-                ret = "Not Implemented";
-            } else {
-                ret = "Server Error";
-            }
+        if(codegenResponse == null || codegenResponse.code == null || codegenResponse.code.isEmpty()){
+            return "";
         }
 
-        return ret;
+        return EnglishReasonPhraseCatalog.INSTANCE.getReason(Integer.parseInt(codegenResponse.code), null);
     }
 }
